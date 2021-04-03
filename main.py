86e8de1dae5f8681af71bf04a6936b2c4b9de03f
itamar <itamardprf@gmail.com>
2021-04-03 12:13:43 -0300

    first working version

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 import tensorflow as tf
+import tensorflow_addons as tfa
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
+IMAGE_SIZE = [256,256]
 
+def decode_image(image):
+    image = (tf.cast(image, tf.float32)/127.5) - 1
+    
+    image = tf.image.resize(image, IMAGE_SIZE)
 
+    image = tf.reshape(image, [1, *IMAGE_SIZE,3])
+
+    return image
 
 def get_image_download_link(img, capt):
     """Generates a link allowing the PIL image to be downloaded
@@ -21,9 +30,6 @@ def get_image_download_link(img, capt):
     return href
 
 st.title("GANdido Portinari")
-
-
-
 
 st.sidebar.title("Options")
 select = st.sidebar.selectbox('Choose the option you want to test!', ["Introduction","Generate Candido's style", "Generate real photos"])
@@ -100,6 +106,33 @@ if select == "Introduction":
 
 elif select == "Generate Candido's style":
     st.header("Generate Candido's style")
+    
+    uploaded_file = st.file_uploader("Choose one image to generate GANdido's style to it", type= ['png', 'jpg'], )
+    
+    gandido = tf.keras.models.load_model("weights/gandido_generator_1.h5", compile=False) # , custom_objects={'InstanceNormalization':tfa.layers.InstanceNormalization}
 
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        tensor = tf.convert_to_tensor(np.array(image))
+        photo = decode_image(tensor)
+
+        prediction = gandido(photo, training=False)[0].numpy()
+
+        st.image(prediction * 0.5 + 0.5)
+ 
 elif select == "Generate real photos":
     st.header("Generate real photos")
+
+    uploaded_file = st.file_uploader("Choose one Candido painting to give it a real world style to it", type= ['png', 'jpg'] )
+    
+    real = tf.keras.models.load_model("weights/gandido_generator_2.h5", compile=False)
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        tensor = tf.convert_to_tensor(np.array(image))
+        photo = decode_image(tensor)
+
+        prediction = real(photo, training=False)[0].numpy()
+
+        st.image(prediction * 0.5 + 0.5)
+ 
